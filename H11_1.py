25e12a3a30243a58f4578ad29a0c9963d0f8cba2
Michelle Maas <michellemaas@live.nl>
2018-07-01 21:24:03 +0100

    Google maps search program

---
+++ H11_1.py
@@ -0,0 +1,18 @@
+#! usr/bin/env python3
+# H11_1.py - Lauches a map in the browser using an address from the command line or clipboard.
+
+import webbrowser, sys, pyperclip 
+
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = ' '.joing(sys.argv[1:])
+else:
+    # Get address from clipboard
+    address = pyperclip.paste()
+
+webbrowser.open('https:/www.google.com/maps/place/' + address)
+
+
+
+
+
